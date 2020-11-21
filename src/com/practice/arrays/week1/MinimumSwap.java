5e95d05a62b3961c8769c83c40eccf6028f2c22f
Anil BT <anilbt@Anils-MacBook-Pro.local>
2020-11-21 13:34:32 +0530

    initial check

---
+++ src/com/practice/arrays/week1/MinimumSwap.java
@@ -0,0 +1,21 @@
+package com.practice.arrays.week1;
+
+/*
+Given an array of n positive integers and a number k. Find the minimum number of swaps required to bring all the numbers less than or equal to k together.
+
+Input:  arr[] = {2, 1, 5, 6, 3}, k = 3
+Output: 1
+
+Explanation:
+To bring elements 2, 1, 3 together, swap
+element '5' with '3' such that final array
+will be-
+arr[] = {2, 1, 3, 6, 5}
+ */
+
+
+public class MinimumSwap {
+    public static void main(String[] args){
+        // code goes here
+    }
+}
